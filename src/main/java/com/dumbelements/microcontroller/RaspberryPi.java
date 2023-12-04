2dde1158f5ef2866b3f4ae425cac2139c014af9e
William Elliman <william.elliman@gmail.com>
2023-12-03 19:31:44 -0600

    Added Raspberry pi formatter

---
+++ src/main/java/com/dumbelements/microcontroller/RaspberryPi.java
@@ -0,0 +1,28 @@
+package com.dumbelements.microcontroller;
+
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+
+import com.dumbelements.beans.BulkLEDStatus;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class RaspberryPi extends Microcontroller{
+
+    protected RaspberryPi(String ip, String port) {
+        super(ip, port);
+        
+    }
+
+    @Override
+    public BodyPublisher formatMessageBody(BulkLEDStatus status) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return BodyPublishers.ofString(mapper.writeValueAsString(status));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+}
