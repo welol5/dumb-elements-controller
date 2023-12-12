6d904d343b30733d5e17214e5aa703c2f504db66
William Elliman <william.elliman@gmail.com>
2023-12-12 16:59:29 -0600

    Updated logging

--- src/main/java/com/dumbelements/led/LEDController.java
+++ src/main/java/com/dumbelements/led/LEDController.java
@@ -1,7 +1,5 @@
 package com.dumbelements.led;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -22,7 +23,7 @@ import com.dumbelements.beans.LEDUpdateRequest;
 @CrossOrigin
 public class LEDController {
 
-    private Logger logger = LogManager.getLogger(LEDController.class);
+    private Logger logger = LoggerFactory.getLogger(LEDController.class);
 
     @Autowired private LEDWorker worker;
     
