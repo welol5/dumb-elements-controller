a2e4f6996e38eb3d8c28c2d2b32c192bc1407491
William Elliman <william.elliman@gmail.com>
2024-02-28 21:06:36 -0600

    Fixed warnings

--- src/main/java/com/dumbelements/DumbElementsControllerApplication.java
+++ src/main/java/com/dumbelements/DumbElementsControllerApplication.java
@@ -1,7 +1,5 @@
 package com.dumbelements;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,8 +8,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class DumbElementsControllerApplication extends SpringBootServletInitializer {
-
-	private static Logger logger = LoggerFactory.getLogger(DumbElementsControllerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DumbElementsControllerApplication.class, args);
